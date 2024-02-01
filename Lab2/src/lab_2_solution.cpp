b8ba3d34aeaecba457fe10a4bb9222ed76c43957
Jackson Mowry <jacksonm@duck.com>
2024-02-01 18:53:35 -0500

    2 done

--- Lab2/src/lab_2_solution.cpp
+++ Lab2/src/lab_2_solution.cpp
@@ -1,3 +1,5 @@
+// Jackson Mowry
+// Feb 01 2024
 #include "lab_2.hpp"
 #include <cfloat>
 #include <cstdint>
@@ -7,21 +9,31 @@
 using namespace std;
 
 unsigned long long whats_my_number_ll() {
+  // Handle either extreme first
   if (am_i_greater_ll(1)) {
+
     return 0;
+  }
+  if (!am_i_greater_ll(numeric_limits<uint64_t>::max())) {
+    return numeric_limits<uint64_t>::max();
   }
   uint64_t curr;
   uint64_t left = 0;
-  uint64_t right = std::numeric_limits<uint64_t>::max();
+  uint64_t right = numeric_limits<uint64_t>::max();
   while (left <= right) {
     curr = ((right - left) / 2) + left;
-    // printf("left: %p, mid: %p, right: %p\n", left, curr, right);
     if (left == right || right - left == 1) {
-      return curr;
+      // At the end we can possible have 2 values, so we return the correct one
+      if (am_i_greater_ll(curr + 1)) {
+        return curr;
+      } else {
+        return curr + 1;
+      }
     }
     if (am_i_greater_ll(curr)) {
       right = curr - 1;
     } else {
+      // We may be equal to the desired value, so don't +1
       left = curr;
     }
   }
@@ -29,25 +41,46 @@ unsigned long long whats_my_number_ll() {
 }
 
 double whats_my_number_d() {
-  // If we move less than 10^-9 end
+  // If we move less than DBL_EPSILON then we end
   double curr;
   double left = 0;
-  double right = 10E9;
+  double right = 10E9; // upper bound given
   while (left <= right) {
     curr = ((right - left) / 2) + left;
-    printf("left: %.10f, mid: %.10f, right: %.10f\n", left, curr, right);
+    if ((curr - left) < DBL_EPSILON || (right - curr) < DBL_EPSILON) {
+      return curr;
+    }
     if (am_i_greater_d(curr)) {
       right = curr - DBL_EPSILON;
     } else {
       left = curr + DBL_EPSILON;
     }
-    if (left == right || (right - left) < 10E-10) {
-      printf("returning %.10f\n", curr);
+    if (left == right || (right - left) < DBL_EPSILON) {
       return curr;
     }
   }
-  cout << "returning 0\n";
   return 0;
 }
 
-unsigned int optimal() { return 0; }
+unsigned int optimal() {
+  double max = 0;
+  uint32_t max_pos;
+  uint32_t curr;
+  uint32_t left = 0;
+  uint32_t right = std::numeric_limits<uint32_t>::max();
+  while (left <= right) {
+    curr = ((right - left) / 2) + left;
+    if (func(curr) > max) {
+      max = func(curr);
+      max_pos = curr;
+    }
+    double mid = func(curr);
+    double next = func(curr + 1);
+    if (mid < next) {
+      left = curr + 1;
+    } else {
+      right = curr - 1;
+    }
+  }
+  return max_pos;
+}
